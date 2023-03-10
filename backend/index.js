b771617caa28301080066dd674ee4397b4bd40ae
Raj Mandloii <rajmandloi01.01@gmail.com>
2023-03-10 22:10:04 +0530

    added path

--- backend/index.js
+++ backend/index.js
@@ -6,13 +6,20 @@ const { connection } = require("./config/db");
 const { authentication } = require("./middlewares/authentication");
 
 const app = express();
+
 app.use(express.json());
+
 app.use(cors());
+
 app.use("/user", userController);
+
 app.use("/product", productController);
+
 // app.use('/orders',productController)
   const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, async () => {
+  
   try {
     await connection;
     console.log("Connected to DB");
